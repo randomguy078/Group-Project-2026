11132a8217a79fc1a743a073a8c33be3a653d967
randomguy078 <mansonchan2332@gmail.com>
2026-04-15 03:41:53 +0800

    Update main.py

--- Task1/main.py
+++ Task1/main.py
@@ -1,1 +1,14 @@
 #main code of the system
+import tkinter as tk
+from login_system import Login
+from gui_main import System
+
+def start():
+    main_window = tk.Tk()
+    System(main_window)
+    main_window.mainloop()
+
+if __name__ == "__main__":
+    tkroot = tk.Tk()
+    Login(tkroot, start)
+    tkroot.mainloop()
